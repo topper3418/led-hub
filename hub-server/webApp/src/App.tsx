4c5d2381a9f4b521734719d933b55bd58fc776e7
topper3418 <67675579+topper3418@users.noreply.github.com>
2024-05-15 22:39:30 -0500

    progress on the frontend

--- hub-server/webApp/src/App.tsx
+++ hub-server/webApp/src/App.tsx
@@ -1,18 +1,39 @@
 import { useState } from 'react'
+import { SketchPicker } from 'react-color';
 import './App.css'
 // simple webpage
 // has a simple button for on/off
 // has a slider for brightness
 // has a color picker for color
 function App() {
+  const [color, setColor] = useState({ r: 171, g: 37, b: 103 });
+  const [brightness, setBrightness] = useState(127);
+  const [on, setOn] = useState(false);
+
+  const handleColorChange = (color: any) => {
+    setColor(color.rgb);
+    console.log('color', color.rgb)
+  };
+
+  const handleBrightnessChange = (event: any) => {
+    setBrightness(event.target.value);
+    console.log('brightness', event.target.value)
+  }
+
+  const handleOnOff = () => {
+    setOn(!on);
+    console.log('on', on)
+  }
   
   return (
-    <div className="root">
+    <div className="wrapper column">
       <header className="App-header">
-        <h1>LED control</h1>
-        <input type="color" />
-        <input type="range" min="0" max="255" />
-        <button>On/Off</button>
+        <div className="column">
+          <h1>LED control</h1>
+            <SketchPicker color={color} onChange={handleColorChange} />
+          <input type="range" min="0" max="255" value={brightness} onChange={handleBrightnessChange} />
+          <button onClick={handleOnOff}>{on ? 'Off' : 'On'}</button>
+        </div>
       </header>
     </div>
   )
