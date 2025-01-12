85acba5e9308c663a6c5f82cca494bcbf180cc32
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-01-12 13:22:25 -0600

    worked on getting it working with docker-compose again

--- hub-webApp/src/config.ts
+++ hub-webApp/src/config.ts
@@ -1,1 +1,3 @@
-export const BACKEND_ROOT_URL = import.meta.env.VITE_BACKEND_ROOT_URL || '/';
+export const BACKEND_HOST = import.meta.env.VITE_BACKEND_HOST || 'localhost';
+export const BACKEND_PORT = import.meta.env.VITE_BACKEND_POST || '2000';
+export const BACKEND_ROOT_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/`;
