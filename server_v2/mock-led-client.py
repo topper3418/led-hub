8ff04194f423f5768cf7290ef393bcd8ed53db71
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-02-16 21:19:24 -0600

    renamed and debugged the mock-led-client

--- server_v2/mock-led-client.py
+++ server_v2/mock-led-client.py
@@ -17,22 +17,20 @@ def generate_random_ip():
     return '.'.join(str(random.randint(0, 255)) for _ in range(4))
 
 
-def handshake(server_endpoint) -> bool | models.Device:
+def fake_write(new_device):
+    print('writing to device, but not really', new_device.model_dump())
+
+
+def handshake(server_endpoint, mac, ip) -> bool | models.Device:
     """sends a request to the server to register the device"""
-    
-    mac = get_mac_address()
-    print(f'got mac: {mac}')
-    
     headers = {
         "content-type": "application/json"
     }
-    
     handshake_data = {
         'mac': mac,
         'type': 'LedStrip',
-        'ip': generate_random_ip(),
+        'ip': ip,
     }
-    
     try:
         print(f'attempting handshake with at "{server_endpoint}')
         res = requests.post(server_endpoint, json={"data": handshake_data}, headers=headers)
@@ -46,9 +44,8 @@ def handshake(server_endpoint) -> bool | models.Device:
             print(f'received error: {error}')
             return False
         device = models.Device(**res_json.get('data'))
-        setattr(device, 'write', lambda: print('writing to device, but not really', device.model_dump()))
         print(f'received response:\n', res_json)
-        device.write()
+        fake_write(device)
         return device
     except Exception as e:
         print('error during handshake:', e)
@@ -57,36 +54,44 @@ def handshake(server_endpoint) -> bool | models.Device:
 
 def do_handshake() -> models.Device:
     handshake_endpoint = SERVER_ENDPOINT + 'devices/'
-    while not (device := handshake(handshake_endpoint)):
+    mac = get_mac_address()
+    ip = generate_random_ip()
+    retries = 0
+    while not (device := handshake(handshake_endpoint, mac, ip)):
+        if retries >= 10:
+            raise Exception('Max retries exceeded')
         time.sleep(1)
+        retries += 1
     if not isinstance(device, models.Device):
         raise Exception('something went wrong and no device was returned. Aborting.')
     return device
 
 
 def get_update(device: models.Device):
-    update_endpoint = SERVER_ENDPOINT + '/devices/' + str(device.id) + '/led_strip'
+    update_endpoint = SERVER_ENDPOINT + 'devices/' + str(device.id)
     # fetch data from server
-    try:
-        response = requests.get(update_endpoint)
-        response_json = response.json()
-        if (error := response_json.get('error')):
-            print('error returned:', error)
-            return
-    except Exception as e:
-        print('an exception was raised while trying to get update', e)
+    response = requests.get(update_endpoint)
+    if not response.status_code == 200:
+        print(f'received a status code of {response.status_code}')
+    response_json = response.json()
+    if (error := response_json.get('error')):
+        print('error returned:', error)
         return
     # load data into object
     try:
-        data = response_json.get('data')
-        new_state = models.LedStrip(**data)
-        device.led_strip = new_state
+        data = response_json.get('data', {})
+        device_data = data.get('device')
+        if not device_data:
+            raise ValueError('no device data returned')
+        print('got data: ', device_data)
+        new_device = models.Device(**device_data)
+        device.led_strip = new_device.led_strip
     except Exception as e:
-        print('an exception was raised while trying to parse response')
+        print('an exception was raised while trying to parse response', e)
         return
     # use that object's data to write to the led strip
     try:
-        device.write()
+        fake_write(device)
     except Exception as e:
         print('an exception was raise dwhile trying to write to device')
 
