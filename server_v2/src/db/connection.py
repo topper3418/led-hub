85f27038311d5172f0265d956ad9ebc659e719b3
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-02-16 21:18:57 -0600

    improved error handling with db initialization

--- server_v2/src/db/connection.py
+++ server_v2/src/db/connection.py
@@ -1,15 +1,19 @@
+import os
 import sqlite3
 
 from src.config import DATABASE_PATH
 from src.logging import get_logger
-
-
 
 logger = get_logger(__name__)
 
 
 def get_connection(connection_path: str = DATABASE_PATH):
     logger.debug("Connecting to database", {"connection_path": connection_path})
+    folder_name = os.path.dirname(connection_path)
+    if not os.path.exists(folder_name):
+        error = f"No folder found matching {folder_name}"
+        logger.error(error)
+        raise ValueError(error)
     connection = sqlite3.connect(connection_path)
     connection.row_factory = sqlite3.Row
     return connection
